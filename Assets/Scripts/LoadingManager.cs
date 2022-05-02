d0df9cbbea39814ae24e468b6e96cad708bae9d5
DyD1124 <ddgp2000@gmail.com>
2022-05-02 16:51:33 -0500

    first commit

--- Assets/Scripts/LoadingManager.cs
+++ Assets/Scripts/LoadingManager.cs
@@ -5,7 +5,11 @@ public class LoadingManager : MonoBehaviour
 {
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-            SceneManager.LoadScene(1);
+        if (Input.GetKeyDown(KeyCode.O))
+            SceneManager.LoadScene(2);
+        if (Input.GetKeyDown(KeyCode.P))
+            SceneManager.LoadScene(3);
+        if (Input.GetKeyDown(KeyCode.L))
+            SceneManager.LoadScene(4);
     }
 }
